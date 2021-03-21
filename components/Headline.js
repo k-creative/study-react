17e56b03e1a87686852f11759c2292e6b5e75fcf
Kota Kawano <kota7654@gmail.com>
2021-03-21 23:15:32 +0900

    ♻️ ヘッドライン部分をコンポーネント化

---
+++ components/Headline.js
@@ -0,0 +1,15 @@
+import styles from "../styles/Home.module.css";
+
+export default function Headline(props) {
+  const { page } = props;
+  return (
+    <div>
+      <h1 className={styles.title}>{page} Page</h1>
+
+      <p className={styles.description}>
+        Get started by editing{" "}
+        <code className={styles.code}>pages/{page}.js</code>
+      </p>
+    </div>
+  );
+}
