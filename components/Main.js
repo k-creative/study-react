fd063fb155a7774de728215fe9169ea5a8e43221
Kota Kawano <kota7654@gmail.com>
2021-03-31 23:35:43 +0900

    💄 スタイルファイルの分割

--- components/Main.js
+++ components/Main.js
@@ -1,13 +1,13 @@
-import styles from "../styles/Home.module.css";
+import classes from "./Main.module.css";
 import { Links } from "./Links";
 import { Headline } from "./Headline";
 
 export function Main(props) {
   const { page } = props;
   return (
-    <main className={styles.main}>
+    <main className={classes.main}>
       <Headline page={page}>
-        <code className={styles.code}>pages/{page}.js</code>
+        <code className={classes.code}>pages/{page}.js</code>
       </Headline>
       <Links />
     </main>
