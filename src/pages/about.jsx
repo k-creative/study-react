b958ad65aacbacff8a9e297e42acf70345190078
k-creative <k.kawano116@gmail.com>
2021-07-05 22:16:00 +0900

    ✨ useMemoについて学びました

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -5,7 +5,7 @@ import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
 
 export default function About(props) {
-  const {count, isShow, handleClick, handleDisplay,text, array, handleChange, handleAdd} = props;
+  const {doubleCount, isShow, handleClick, handleDisplay,text, array, handleChange, handleAdd} = props;
   
   return (
     <div className={styles.container}>
@@ -13,18 +13,19 @@ export default function About(props) {
         <title>About Page</title>
       </Head>
       <Header />
-      {isShow ? <h1>{count}</h1> : null }
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>
         {isShow ? "非表示" : "表示"}
-        </button>
+      </button>
+      {isShow ? <h1>{doubleCount}</h1> : null }
+      <hr />
       <input type="text" value={text} onChange={handleChange} />
       <button onClick={handleAdd}>追加</button>
-      <ul>{array.map(item => {
+      <ol>{array.map(item => {
         return(
         <li key={item}>{item}</li>
         )
-      })}</ul>
+      })}</ol>
       <Main page="about" />
 
       <Footer />
