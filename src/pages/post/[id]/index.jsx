4db76496046b5f694031ca2c1cf014227467e66b
Kota Kawano <k.kawano116@gmail.com>
2021-09-07 00:21:18 +0900

    ✨ ユーザーのfetch、fetcherの共通化

--- src/pages/post/[id]/index.jsx
+++ src/pages/post/[id]/index.jsx
@@ -3,17 +3,15 @@ import styles from "src/styles/Home.module.css";
 import { Header } from "src/components/Header";
 import { Posts } from "src/components/Posts";
 import { useRouter } from "next/router";
+import useSWR from "swr";
+import { usePost } from "src/hooks/usePost";
+import { Post } from "src/components/Post";
 
 const PostId = () => {
-  const router = useRouter();
-
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Index Page</title>
-      </Head>
       <Header />
-      <div>{router.query.id}</div>
+      <Post />
     </div>
   );
 };
